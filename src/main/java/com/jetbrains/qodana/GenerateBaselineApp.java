a117a526cc6fc12a324990b7c546f0a2e56a17f5
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2021-06-11 10:26:10 +0300

    Add output of logging messages to STDOUT.

--- src/main/java/com/jetbrains/qodana/GenerateBaselineApp.java
+++ src/main/java/com/jetbrains/qodana/GenerateBaselineApp.java
@@ -5,20 +5,34 @@ import com.jetbrains.qodana.sarif.baseline.BaselineCalculation;
 import com.jetbrains.qodana.sarif.model.SarifReport;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class GenerateBaselineApp {
     public static void main(String[] args) throws IOException {
-        if (args.length < 3 ) System.out.println("Format: %reportFilename% %baselineFilename% %outFilename%[--failOnNew] [--failOnAbsent]");
-        String reportFilename = args[0];
-        String baselineFilename = args[1];
-        String outFilename = args[1];
-        SarifReport report = SarifUtil.readReport(Paths.get(reportFilename));
-        SarifReport baseline = SarifUtil.readReport(Paths.get(baselineFilename));
-        BaselineCalculation baselineResult = BaselineCalculation.compare(report, baseline);
-        SarifUtil.writeReport(Paths.get(outFilename), report);
-        if (Arrays.asList(args).contains("--failOnNew") && baselineResult.getNewResults() > 0) System.exit(255);
-        if (Arrays.asList(args).contains("--failOnAbsent") && baselineResult.getAbsentResults() > 0) System.exit(255);
+        if (args.length < 3) {
+            System.out.println("Format: %reportPath% %baselinePath% %outFilename%[--failOnNew] [--failOnAbsent]");
+            System.exit(1);
+        }
+        Path reportPath = Paths.get(args[0]);
+        Path baselinePath = Paths.get(args[1]);
+        String outFilename = args[2];
+        SarifReport report = SarifUtil.readReport(reportPath);
+        SarifReport baseline = SarifUtil.readReport(baselinePath);
+        System.out.println("Starting baseline calculation. Report: " + reportPath.toAbsolutePath() + "; Baseline: " + baselinePath.toAbsolutePath());
+        BaselineCalculation calculation = BaselineCalculation.compare(report, baseline);
+        System.out.printf(
+                "Baseline comparison result - UNCHANGED: %s, NEW: %s, ABSENT: %s\n",
+                calculation.getUnchangedResults(),
+                calculation.getNewResults(),
+                calculation.getAbsentResults()
+        );
+
+        Path outPath = Paths.get(outFilename);
+        System.out.println("Writing report with baseline comparison to: " + outPath.toAbsolutePath());
+        SarifUtil.writeReport(outPath, report);
+        if (Arrays.asList(args).contains("--failOnNew") && calculation.getNewResults() > 0) System.exit(255);
+        if (Arrays.asList(args).contains("--failOnAbsent") && calculation.getAbsentResults() > 0) System.exit(255);
     }
 }
