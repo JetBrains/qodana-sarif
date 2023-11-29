b673bf09b2e75907f998c7c765744f29b3b7b349
Johannes Koenen <johannes.koenen@jetbrains.com>
2023-11-29 14:12:47 +0100

    Refactor BaselineCliTest
    - Capture whole program output instead of last-line only
    - Simplify temp-file setup
    - Clearly separate between stdout and stderr

--- baseline-cli/src/test/kotlin/BaselineCliTest.kt
+++ baseline-cli/src/test/kotlin/BaselineCliTest.kt
@@ -1,22 +1,21 @@
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 import kotlin.io.path.Path
-import kotlin.io.path.readText
 
 class BaselineCliTest {
 
-    private lateinit var sarif: File
-
-    @BeforeEach
-    fun setup() {
-        sarif = File.createTempFile("temp", ".sarif")
-        sarif.writeText(Path("src/test/resources/report.equal.sarif.json").readText())
-        sarif.deleteOnExit()
+    private val sarif = run {
+        val tmp = Files.createTempFile(null, ".sarif")
+        Files.copy(Path("src/test/resources/report.equal.sarif.json"), tmp, StandardCopyOption.REPLACE_EXISTING)
+        tmp.toFile().also(File::deleteOnExit).absolutePath
     }
 
+    private val stdout = StringBuilder()
+    private val stderr = StringBuilder()
 
     @Test
     fun `test when sarifReport does not exist in the provided path`() {
@@ -24,40 +23,31 @@ class BaselineCliTest {
         val map = mutableMapOf<String, String>().apply {
             this["sarifReport"] = "nonExistentPath.sarif"
         }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
-        }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
         assertEquals(ERROR_EXIT, exitCode)
-        assertEquals("Please provide a valid SARIF report path", printedMessage)
+        assertEquals("Please provide a valid SARIF report path", stderr.toString())
     }
 
     @Test
     fun `test when there is an error reading sarifReport`() {
         // Arrange
-        val map = mutableMapOf<String, String>().apply{
+        val map = mutableMapOf<String, String>().apply {
             this["sarifReport"] = Path("src/test/resources/corrupted.sarif.json").toString()
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
-        assertNotNull(printedMessage)
-        assertTrue(printedMessage!!.startsWith("Error reading SARIF report"))
+        assertTrue(stderr.startsWith("Error reading SARIF report"))
         assertEquals(ERROR_EXIT, exitCode)
     }
 
@@ -65,44 +55,35 @@ class BaselineCliTest {
     fun `test when baselineReport path is provided and file does not exist`() {
         // Arrange
         val map = mutableMapOf<String, String>().apply {
-            this["sarifReport"] = sarif.absolutePath.toString()
+            this["sarifReport"] = sarif
             this["baselineReport"] = "nonExistentBaselineReport.sarif"
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
         assertEquals(ERROR_EXIT, exitCode)
-        assertEquals("Please provide valid baseline report path", printedMessage)
+        assertEquals("Please provide valid baseline report path", stderr.toString())
     }
 
     @Test
     fun `test when there is a error reading baselineReport`() {
         // Arrange
-        val map = mutableMapOf<String, String>().apply{
-            this["sarifReport"] = sarif.absolutePath.toString()
+        val map = mutableMapOf<String, String>().apply {
+            this["sarifReport"] = sarif
             this["baselineReport"] = Path("src/test/resources/corrupted.sarif.json").toString()
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
-        assertNotNull(printedMessage)
-        assertTrue(printedMessage!!.startsWith("Error reading baseline report"))
+        assertTrue(stderr.startsWith("Error reading baseline report"))
         assertEquals(ERROR_EXIT, exitCode)
     }
 
@@ -110,93 +91,72 @@ class BaselineCliTest {
     fun `test when failThreshold is not present and results count is less than the failThreshold default value`() {
         // Arrange
         val map = mutableMapOf<String, String>().apply {
-            this["sarifReport"] = sarif.absolutePath.toString()
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
+            this["sarifReport"] = sarif
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
         assertEquals(0, exitCode)
-        assertTrue(printedMessage == null || !printedMessage!!.contains("is greater than the threshold"))
+        assertTrue(!stdout.contains("is greater than the threshold"))
     }
 
     @Test
     fun `test when failThreshold is provided and results count in sarifReport is more than failThreshold`() {
         // Arrange
         val map = mutableMapOf<String, String>().apply {
-            this["sarifReport"] = sarif.absolutePath.toString()
+            this["sarifReport"] = sarif
             this["failThreshold"] = "0"
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
         assertEquals(THRESHOLD_EXIT, exitCode)
-        assertTrue(printedMessage!!.contains("New problems count") && printedMessage!!.contains("is greater than the threshold"))
+        assertTrue(stderr.contains("New problems count") && stderr.contains("is greater than the threshold"))
     }
 
     @Test
     fun `test when failThreshold is provided and newResults count in baselineCalculation is more than failThreshold`() {
-        val tempFile = File.createTempFile("temp", ".sarif")
-        tempFile.writeText(Path("src/test/resources/report.equal.sarif.json").readText())
-        tempFile.deleteOnExit()
         // Arrange
         val map = mutableMapOf<String, String>().apply {
-            this["sarifReport"] = tempFile.absolutePath.toString()
+            this["sarifReport"] = sarif
             this["baselineReport"] = Path("src/test/resources/empty.sarif.json").toString()
             this["failThreshold"] = "1"
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String) -> Unit = {
-            printedMessage = it
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
         assertEquals(THRESHOLD_EXIT, exitCode)
-        assertTrue(printedMessage!!.contains("New problems count") && printedMessage!!.contains("is greater than the threshold"))
+        assertTrue(stderr.contains("New problems count") && stderr.contains("is greater than the threshold"))
     }
 
     @Test
     fun `test when failThreshold is not provided or is less than newResults count in baselineCalculation`() {
-        val tempFile = File.createTempFile("temp", ".sarif")
-        tempFile.writeText(Path("src/test/resources/report.equal.sarif.json").readText())
-        tempFile.deleteOnExit()
         // Arrange
         val map = mutableMapOf<String, String>().apply {
-            this["sarifReport"] = tempFile.absolutePath.toString()
+            this["sarifReport"] = sarif
             this["baselineReport"] = Path("src/test/resources/empty.sarif.json").toString()
-        }
-        var printedMessage: String? = null
-        val testCliPrinter: (String)-> Unit = {
-            printedMessage = it
         }
 
         // Act
         val exitCode = assertDoesNotThrow {
-            BaselineCli.process(map, testCliPrinter, testCliPrinter)
+            BaselineCli.process(map, stdout::append, stderr::append)
         }
 
         // Assert
         assertEquals(0, exitCode) // if the failThreshold is not provided, the function should not return a failure
-        assertFalse(printedMessage!!.contains("New problems count") && printedMessage!!.contains("is greater than the threshold"))
+        assertFalse(stdout.contains("New problems count") && stdout.contains("is greater than the threshold"))
     }
+
 }
