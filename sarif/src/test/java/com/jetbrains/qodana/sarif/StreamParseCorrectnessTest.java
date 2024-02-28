4fdd6f035a27e791559cf2acad8bc18c6887c705
Theodor Port <theodor.port@jetbrains.com>
2024-02-28 19:07:38 +0100

    fix(sarif): QD-8410: Fix tests

--- sarif/src/test/java/com/jetbrains/qodana/sarif/StreamParseCorrectnessTest.java
+++ sarif/src/test/java/com/jetbrains/qodana/sarif/StreamParseCorrectnessTest.java
@@ -1,6 +1,9 @@
 package com.jetbrains.qodana.sarif;
 
+import com.google.gson.Gson;
+import com.jetbrains.qodana.sarif.model.PropertyBag;
 import com.jetbrains.qodana.sarif.model.Result;
+import com.jetbrains.qodana.sarif.model.Run;
 import com.jetbrains.qodana.sarif.model.SarifReport;
 import com.jetbrains.qodana.sarif.model.streaming.IndexedResult;
 import com.jetbrains.qodana.sarif.model.streaming.ResultLocation;
@@ -8,11 +11,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StreamParseCorrectnessTest extends StreamParseTest {
+    private static final Gson gson = new Gson();
     @Test
     public void testStreamParseMakeSameAsSimpleParseReadingResultsOneByOne() {
         forEachInput(inputPath -> {
@@ -24,13 +30,17 @@ public class StreamParseCorrectnessTest extends StreamParseTest {
                 new StreamingSarifReport(actualReport, () -> makeReader(inputPath)),
                 (expected, actual, index) -> {
                     Iterator<Result> expectedResults = expected.getResults().iterator();
-                    Iterator<Result> actualResults = SarifUtil.lazyReadResults(actual.getReader(), index);
-                    while (expectedResults.hasNext()) {
-                        assertTrue(actualResults.hasNext());
-                        assertEquals(expectedResults.next(), actualResults.next());
-                    }
-                    assertEquals(expected.withResults(null), actual.getRun().withResults(null));
-                    assertFalse(actualResults.hasNext());
+                    actual.getReader(reader -> {
+                        Iterator<Result> actualResults = SarifUtil.lazyReadResults(reader, index);
+                        while (expectedResults.hasNext()) {
+                            assertTrue(actualResults.hasNext());
+                            assertEquals(expectedResults.next(), actualResults.next());
+                        }
+                        assertEquals(expected.withResults(null).withProperties(null), actual.getRun().withResults(null).withProperties(null));
+                        assertFalse(actualResults.hasNext());
+
+                    });
+                    assertRunPropertyBag(expected, actual, index);
                 }
             );
         });
@@ -48,19 +58,22 @@ public class StreamParseCorrectnessTest extends StreamParseTest {
                 expectedReport,
                 new StreamingSarifReport(actualReport, () -> makeReader(inputPath)),
                 (expected, actual, index) -> {
-                    Iterator<Result> expectedResults = expected.getResults().iterator();
-                    Iterator<IndexedResult> actualResults = SarifUtil.lazyReadIndexedResults(actual.getReader());
-                    while (expectedResults.hasNext()) {
-                        IndexedResult indexedResult;
-                        do {
-                            assertTrue(actualResults.hasNext());
-                            indexedResult = actualResults.next();
-                        } while (indexedResult.getIndex() != index);
-                        assertEquals((int)index, indexedResult.getIndex());
-                        assertEquals(expectedResults.next(), indexedResult.getResult());
-                        anyLeftAtLastRun[0] = actualResults.hasNext();
-                    }
-                    assertEquals(expected.withResults(null), actual.getRun().withResults(null));
+                    actual.getReader(reader -> {
+                        Iterator<Result> expectedResults = expected.getResults().iterator();
+                        Iterator<IndexedResult> actualResults = SarifUtil.lazyReadIndexedResults(reader);
+                        while (expectedResults.hasNext()) {
+                            IndexedResult indexedResult;
+                            do {
+                                assertTrue(actualResults.hasNext());
+                                indexedResult = actualResults.next();
+                            } while (indexedResult.getIndex() != index);
+                            assertEquals((int) index, indexedResult.getIndex());
+                            assertEquals(expectedResults.next(), indexedResult.getResult());
+                            anyLeftAtLastRun[0] = actualResults.hasNext();
+                        }
+                        assertEquals(expected.withResults(null).withProperties(null), actual.getRun().withResults(null).withProperties(null));
+                    });
+                    assertRunPropertyBag(expected, actual, index);
                 }
             );
             assertFalse(anyLeftAtLastRun[0]);
@@ -85,6 +98,29 @@ public class StreamParseCorrectnessTest extends StreamParseTest {
                 assertEquals(expectedResults.next(), actualResults.next());
             }
             assertFalse(actualResults.hasNext());
+        }
+    }
+
+    private static void assertRunPropertyBag(Run expected, StreamingRun actual, Integer runIndex) throws IOException {
+        if (expected.getProperties() != null) {
+            actual.getReader(reader -> {
+                Iterator<Object> expectedSanityResults = ((List<Object>) expected.getProperties().getOrDefault(
+                        "qodana.sanity.results",
+                        Collections.emptyList()
+                )).iterator();
+                Iterator<Result> actualSanityResults = SarifUtil.lazyReadResultsFromLocation(
+                        reader,
+                        new ResultLocation.InProperties(runIndex, "qodana.sanity.results")
+                );
+                while (expectedSanityResults.hasNext()) {
+                    assertTrue(actualSanityResults.hasNext());
+                    Result expectedSanityResult = gson.fromJson(gson.toJson(expectedSanityResults.next()), Result.class);
+                    assertEquals(expectedSanityResult, actualSanityResults.next());
+                }
+                PropertyBag expectedProperties = expected.getProperties();
+                expectedProperties.remove("qodana.sanity.results");
+                assertEquals(expectedProperties, actual.getRun().getProperties());
+            });
         }
     }
 }
