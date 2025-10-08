f4a6557877bc0709674801c0ffdbb259e5fd091e
Andrei Iurko <andrei.iurko@jetbrains.com>
2025-10-08 11:03:03 +0300

    QD-10721 add the ability to specify originalUriBaseIds in the run object

--- sarif/src/main/java/com/jetbrains/qodana/sarif/model/OriginalUriBaseIds.java
+++ sarif/src/main/java/com/jetbrains/qodana/sarif/model/OriginalUriBaseIds.java
@@ -1,16 +1,87 @@
 package com.jetbrains.qodana.sarif.model;
 
+import java.util.*;
+
 
 /**
  * The artifact location specified by each uriBaseId symbol on the machine where the tool originally ran.
+ * Each property name designates a URI base id, and each property value is an artifactLocation object
+ * that specifies the absolute URI of that URI base id on the machine where the SARIF producer ran.
  */
-public class OriginalUriBaseIds {
+public class OriginalUriBaseIds implements Map<String, ArtifactLocation> {
 
+    private final Map<String, ArtifactLocation> additionalProperties = new HashMap<>();
+
+    @Override
+    public int size() {
+        return additionalProperties.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return additionalProperties.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return additionalProperties.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return additionalProperties.containsValue(value);
+    }
+
+    @Override
+    public ArtifactLocation get(Object key) {
+        return additionalProperties.get(key);
+    }
+
+    @Override
+    public ArtifactLocation put(String key, ArtifactLocation value) {
+        return additionalProperties.put(key, value);
+    }
+
+    @Override
+    public ArtifactLocation remove(Object key) {
+        return additionalProperties.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends ArtifactLocation> m) {
+        additionalProperties.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        additionalProperties.clear();
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return additionalProperties.keySet();
+    }
+
+    @Override
+    public Collection<ArtifactLocation> values() {
+        return additionalProperties.values();
+    }
+
+    @Override
+    public Set<Entry<String, ArtifactLocation>> entrySet() {
+        return additionalProperties.entrySet();
+    }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(OriginalUriBaseIds.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        for (Map.Entry<String, ArtifactLocation> entry : additionalProperties.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append('=');
+            sb.append(((entry.getValue() == null) ? "<null>" : entry.getValue()));
+            sb.append(',');
+        }
         if (sb.charAt((sb.length() - 1)) == ',') {
             sb.setCharAt((sb.length() - 1), ']');
         } else {
@@ -21,7 +92,7 @@ public class OriginalUriBaseIds {
 
     @Override
     public int hashCode() {
-        return 1;
+        return additionalProperties.hashCode();
     }
 
     @Override
@@ -33,7 +104,7 @@ public class OriginalUriBaseIds {
             return false;
         }
         OriginalUriBaseIds rhs = ((OriginalUriBaseIds) other);
-        return true;
+        return additionalProperties.equals(rhs.additionalProperties);
     }
 
 }
